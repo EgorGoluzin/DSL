2d813f9c6dc11c5b9f729a690faba683999ae5d9
AlexSoulJk <alex.solomatow2016@yandex.ru>
2025-04-13 12:02:05 +0300

    Add wirth tool for rule

---
+++ DSLTools/core/vertex_tool.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from DSLTools.models.legacy_for_wirth import NodeLegacy
+
+
+def generate_graph_structure(vertex: list[NodeLegacy], matrix: list[list]) -> NodeLegacy:
+    """Здесь будет функция по переводу в структуру, повторяющую GetSyntaxInfo"""
+    pass
+
+
+def generate_wirth_by_rule(vertex: list[NodeLegacy], matrix: list[list], path_to_save: Path) -> None:
+    """Здесь будет перевод правила в диаграмму Вирта."""
+    pass
